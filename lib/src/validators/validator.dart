3902509f7088647e0e9dfbeac54b51d020b0e2d7
Kenneth <kenneth.ng.5226@outlook.com>
2021-05-07 17:25:32 +0100

    Export Validator

--- lib/src/validators/validator.dart
+++ lib/src/validators/validator.dart
@@ -2,6 +2,8 @@
 /// and returns error message if any, or an empty string.
 typedef ValidatorFunc<T> = String Function(T? value);
 
+/// Validates a given value. Custom validators should implement this class
+/// so that they can interface with Former internals.
 abstract class Validator<T> {
   /// The error message as a result of a failed validation.
   /// This is empty if the previous validation is passed.
--- lib/validators.dart
+++ lib/validators.dart
@@ -1,6 +1,7 @@
 /// This library exports various form validators.
 library former.validators;
 
+export 'src/validators/validator.dart';
 export 'src/validators/string_validator.dart';
 export 'src/validators/number_validator.dart';
 export 'src/validators/bool_validator.dart';
